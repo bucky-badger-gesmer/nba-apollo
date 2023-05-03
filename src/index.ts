4a1aa7a20551862f1325046ae20934aab478e6fc
bucky-badger-gesmer <ajgesmer@comcast.net>
2023-05-03 09:32:41 -0500

    update src/index.ts to use ContextValue

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs, resolvers } from "./models";
+import { NbaAPI } from "./dataSources/NbaAPI/NbaAPI";
+
+interface ContextValue {
+  dataSources: {
+    nbaAPI: NbaAPI;
+  };
+}
 
 const startApolloServer = async () => {
-  const server = new ApolloServer({
+  const server = new ApolloServer<ContextValue>({
     typeDefs,
     resolvers,
   });
 
   const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+      return {
+        dataSources: {
+          nbaAPI: new NbaAPI({ cache }),
+        },
+      };
+    },
+
     listen: { port: Number(process.env.PORT) || 4000 },
   });
 
