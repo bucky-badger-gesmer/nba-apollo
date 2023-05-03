a231938c436dd0b385481d017ea4ac565e17ee41
bucky-badger-gesmer <ajgesmer@comcast.net>
2023-05-03 16:13:10 -0500

    add FranchiseHistory to typeDefs

--- src/models/nba/nbaTypeDefs.ts
+++ src/models/nba/nbaTypeDefs.ts
@@ -2,15 +2,39 @@ import { gql } from "apollo-server";
 
 export const typeDefs = gql`
   extend type Query {
-    nba: Nba
+    nba: NBA
   }
 
-  type Nba {
+  type NBA {
+    franchiseHistory: FranchiseHistory
     players: [Player]
   }
 
+  type FranchiseHistory {
+    franchises: [Franchise]
+    defunctFranchises: [Franchise]
+  }
+
+  type Franchise {
+    leagueId: String
+    teamId: ID
+    teamCity: String
+    teamName: String
+    startYear: String
+    endYear: String
+    years: Int
+    games: Int
+    wins: Int
+    losses: Int
+    winPct: Float
+    playoffAppearances: Int
+    divisionTitles: Int
+    conferenceTitles: Int
+    leagueTitles: Int
+  }
+
   type Player {
-    id: String
+    id: ID
     displayLastCommaFirst: String
     displayFirstLast: String
     fromYear: String
