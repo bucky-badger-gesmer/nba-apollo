cb58c198b7dfeefcff2c64e1d9f73005895f8499
bucky-badger-gesmer <ajgesmer@comcast.net>
2023-05-03 09:34:12 -0500

    edit NbaAPI to use apiCall

--- src/dataSources/NbaAPI/NbaAPI.ts
+++ src/dataSources/NbaAPI/NbaAPI.ts
@@ -1,37 +1,17 @@
-import { DataSourceConfig } from "apollo-datasource";
-import { NBAClient } from "../../api/nba";
-import { SportQLContext } from "../../types";
-import { SportQLDataSource } from "../SportQLDataSource";
+import { RESTDataSource } from "@apollo/datasource-rest";
+import { apiCall } from "../../models/helpers";
 
-export class NbaAPI extends SportQLDataSource {
-  constructor() {
-    super();
-  }
-
-  private Nba: NBAClient;
-
-  initialize(args: DataSourceConfig<SportQLContext>): void {
-    super.initialize(args);
-    this.Nba = new NBAClient({
-      HEADERS: {
-        ...this.headers,
-        accept: "*/*",
-        host: "stats.nba.com",
-        origin: "https://www.nba.com",
-        referer: "https://www.nba.com",
-      },
-    });
-  }
-
-  commonAllPlayers = async () => {
+export class NbaAPI extends RESTDataSource {
+  async getPlayers() {
     try {
-      console.log("trying...");
-      return await this.Nba.playerController.commonAllPlayers({
-        leagueId: "00",
-      });
+      const data = (await apiCall("commonAllPlayers")).data;
+      return {
+        resource: data.resource,
+        parameters: data.parameters,
+        resultSets: data.resultSets,
+      };
     } catch (error) {
       console.log("error", error);
-      throw new Error(error);
     }
-  };
+  }
 }
