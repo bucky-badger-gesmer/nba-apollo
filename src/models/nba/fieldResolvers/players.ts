dd7301ab4f2991b6ca71e42ade2c59d9161a3a15
bucky-badger-gesmer <ajgesmer@comcast.net>
2023-05-03 09:31:51 -0500

    add players resolver

---
+++ src/models/nba/fieldResolvers/players.ts
@@ -0,0 +1,30 @@
+import { NbaResolvers, Player } from "../../../generated/graphql";
+import { SportQLContext } from "../../../types";
+
+export const players: NbaResolvers<SportQLContext>["players"] = async (
+  _,
+  __,
+  { dataSources }
+) => {
+  const getPlayersResult = await dataSources.nbaAPI.getPlayers();
+
+  const resultSets = getPlayersResult.resultSets;
+  const commonAllPlayers = resultSets[0];
+  const headers = commonAllPlayers.headers;
+  const rowSet = commonAllPlayers.rowSet;
+
+  const players: Player[] = [];
+  rowSet.forEach((row) => {
+    players.push({
+      id: row[headers.indexOf("PERSON_ID")],
+      displayLastCommaFirst: row[headers.indexOf("DISPLAY_LAST_COMMA_FIRST")],
+      displayFirstLast: row[headers.indexOf("DISPLAY_FIRST_LAST")],
+      fromYear: row[headers.indexOf("FROM_YEAR")],
+      toYear: row[headers.indexOf("TO_YEAR")],
+      playerCode: row[headers.indexOf("PLAYERCODE")],
+      playerSlug: row[headers.indexOf("PLAYER_SLUG")],
+    });
+  });
+
+  return players;
+};
